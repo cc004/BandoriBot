4840704c916917d5ff6a3d4968e6fefe7caf41b7
root <root@DESKTOP-BFTLO8O.localdomain>
2020-12-26 14:54:26 +0800

    remove mirai-csharp folder to prepare for submodules

--- Mirai-CSharp/Mirai-CSharp/Utility/JsonSerializeOptionsFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json;
-
-namespace Mirai_CSharp.Utility
-{
-    public static class JsonSerializeOptionsFactory
-    {
-        public static JsonSerializerOptions IgnoreNulls => new JsonSerializerOptions { IgnoreNullValues = true };
-    }
-}
